5cd48608f5f22b1ed9eeb2a4dd01e7eb33225047
yaoyulong <122012225@qq.com>
2022-12-14 19:38:39 +0800

    up

---
+++ js/img-01-14148a0b.js
@@ -0,0 +1,1 @@
+var a="./assets/img-01-fcdae367.png";export{a as _};
---
+++ js/index-ee5c5d5e.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r,b as t}from"../default/default-31c6562e.js";const a={},i=[t("label",{for:""},[t("input",{type:"file",name:"cover",accept:"image/*",id:"file",onfocus:"this.blur()"})],-1)];var l=e(a,[["render",function(e,t){return o(),r("div",null,i)}],["__file","E:/other/work/work/work/src/views/photo/index.vue"]]);export{l as default};
