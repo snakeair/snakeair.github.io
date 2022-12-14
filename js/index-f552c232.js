aaf101c24cae90e01955599648dfa28a811d3abd
yaoyulong <122012225@qq.com>
2022-12-14 19:41:09 +0800

    up

---
+++ js/index-f552c232.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r,b as t}from"../default/default-76b513c9.js";const a={},i=[t("label",{for:""},[t("input",{type:"file",name:"cover",accept:"image/*",id:"file",onfocus:"this.blur()"})],-1)];var l=e(a,[["render",function(e,t){return o(),r("div",null,i)}],["__file","E:/other/work/work/work/src/views/photo/index.vue"]]);export{l as default};
