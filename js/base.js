630cd79058ce992f00ed922b28739bf3dad1eba3
yaoyulong <122012225@qq.com>
2023-07-22 15:08:46 +0800

    langpage

--- js/base.js
+++ js/base.js
@@ -2,8 +2,33 @@ function submitFn() {
   if ($(".layer-msg").is(":visible")) {
     return false;
   }
-  console.log(form.name.value);
-  errFn("密码错误");
+  // if (form.name.value.trim() == "") {
+  //   errFn("请输入您的姓名");
+  //   return false;
+  // }
+  // if (!phoneFn(form.mobile.value.trim())) {
+  //   errFn("请输入正确的手机号码");
+  //   return false;
+  // }
+  // if (form.code.value.trim() == "") {
+  //   errFn("请输入验证码");
+  //   return false;
+  // }
+  if (!form.money.value || form.money.value == "") {
+    errFn("请选择资金需求");
+    return false;
+  }
+  let moneySize = [];
+  form.moneySize.forEach(function (el) {
+    if (el.checked) {
+      moneySize.push(el.value);
+    }
+  });
+  if (!moneySize || moneySize.length == 0) {
+    errFn("请选择资质");
+    return false;
+  }
+
   return false;
 }
 
@@ -15,7 +40,7 @@ function errFn(msg) {
   }, 2000);
   timer = setTimeout(function () {
     $(".layer-msg").removeClass("animate__fadeOutDown ").hide();
-  }, 3000);
+  }, 2500);
 }
 
 $(function () {
@@ -30,4 +55,29 @@ $(function () {
   $(".layer-pact .layer-btn").click(function () {
     $(".cloud, .layer-pact").slideUp();
   });
+
+  $(".form-main .get-code").click(function () {
+    if (!phoneFn($(".form-main .mobile").val())) {
+      errFn("请输入正确的手机号码");
+      return false;
+    }
+    $(".form-main .get-code").hide();
+    $(".form-main .wait-code").show();
+    var timer = setInterval(function () {
+      let size = $(".form-main .wait-code .num").text();
+      if (size > 1) {
+        size--;
+      } else {
+        size = 60;
+        $(".form-main .get-code").show();
+        $(".form-main .wait-code").hide();
+      }
+      $(".form-main .wait-code .num").text(size);
+    }, 1000);
+  });
 });
+
+function phoneFn(val) {
+  const reg = /^(1[3|4|5|6|7|8|9])[\d]{9}$/g;
+  return reg.test(val);
+}
