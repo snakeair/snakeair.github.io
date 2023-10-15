c971e39bf6fdc584d886d73a33db8cc92495f6c1
yaoyulong <122012225@qq.com>
2023-10-15 20:05:04 +0800

    third

---
+++ js/agreement3-5650e295.js
@@ -0,0 +1,1 @@
+import{p as a}from"./pactThree-54d44469.js";import{d as e,e as s,f as t,h as d,_ as p}from"../default/default-6ef38c5b.js";const r={class:"agreement-page"},m=p(e({__name:"agreement3",setup:e=>(e,p)=>(s(),t("div",r,[d(a)]))}),[["__scopeId","data-v-b914d601"]]);export{m as default};
