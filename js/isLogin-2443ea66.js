0339e77a2b43b5ef2260cebb3de61ad0d293b012
yaoyulong <122012225@qq.com>
2023-10-20 19:10:39 +0800

    协议

---
+++ js/isLogin-2443ea66.js
@@ -0,0 +1,1 @@
+import{a as e,n as t,$ as n}from"../default/default-10ae9c6f.js";function s(){const s=e();return{isLoginFn:()=>{let e=t.get("userId");e&&""!=e||s.push({path:"/"})},getLoginInfoFn:()=>{n("/api/user/selectUserInfo",{}).then((e=>{}))}}}export{s as i};
