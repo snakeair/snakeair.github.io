bf952b18e1a250b23d0e1c068e4eb6f43833f5d1
yaoyulong <122012225@qq.com>
2025-09-21 10:21:37 +0800

    1

---
+++ js/code-d7bfbc57.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/code-f548a2fd.png",import.meta.url).href;export{e as default};
---
+++ js/index-70a4c785.js
@@ -0,0 +1,1 @@
+import{az as s}from"../default/default-5fc83b23.js";const e=s("main",{state:()=>({msg:"hello",channel:"ceshi1",users:{},userMobile:"",userCheckedPolicy:!1}),getters:{},actions:{actionMsg(s){this.msg=s}}});export{e as i};
