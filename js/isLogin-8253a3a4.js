ddbbb7c3aa5f74a0370547ab51fef33b8bd81b22
yaoyulong <122012225@qq.com>
2025-09-22 20:12:53 +0800

    1

---
+++ js/isLogin-8253a3a4.js
@@ -0,0 +1,1 @@
+import{p as o}from"../default/default-3df10792.js";import{a as s}from"./js.cookie-d59fc71c.js";import{$ as t}from"./axios-0703b32e.js";function e(){const e=o();return{isLoginFn:()=>{let o=s.get("userId");o&&""!=o||e.push({path:"/"})},getLoginInfoFn:()=>{t("/api/user/selectUserInfo",{}).then(o=>{})}}}export{e as i};
