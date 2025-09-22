56e3e29aee957a732c05296c23a3ab7ddd7a1f9c
yaoyulong <122012225@qq.com>
2025-09-22 20:24:58 +0800

    1

---
+++ js/agreement3-8aeddf06.js
@@ -0,0 +1,1 @@
+import{p as e}from"./pactThree-f5ac4ea1.js";import{d as a,b as s,e as t,j as o,x as p}from"../default/default-5731c765.js";import"./js.cookie-d59fc71c.js";const r={class:"agreement-page"},m=p(a({__name:"agreement3",setup:a=>(a,p)=>(s(),t("div",r,[o(e)]))}),[["__scopeId","data-v-be7b02ee"]]);export{m as default};
---
+++ js/isLogin-8bd3de41.js
@@ -0,0 +1,1 @@
+import{p as o}from"../default/default-5731c765.js";import{a as s}from"./js.cookie-d59fc71c.js";import{$ as t}from"./axios-b6fad79f.js";function e(){const e=o();return{isLoginFn:()=>{let o=s.get("userId");o&&""!=o||e.push({path:"/"})},getLoginInfoFn:()=>{t("/api/user/selectUserInfo",{}).then(o=>{})}}}export{e as i};
