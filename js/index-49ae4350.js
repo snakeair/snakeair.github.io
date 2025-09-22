1d4dd23f27f590c7d3d9526c8f7363f12966dd0f
yaoyulong <122012225@qq.com>
2025-09-22 20:38:22 +0800

    1

---
+++ js/index-49ae4350.js
@@ -0,0 +1,1 @@
+import{az as s}from"../default/default-9bc3301a.js";const e=s("main",{state:()=>({msg:"hello",channel:"ceshi1",users:{},userMobile:"",userCheckedPolicy:!1}),getters:{},actions:{actionMsg(s){this.msg=s}}});export{e as i};
